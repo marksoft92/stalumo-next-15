2e11e68368b4f9b9105ee86f510e6a1c4bdada7e
Dawid Bieńkowski <marksoft1992@gmail.com>
2025-03-31 15:06:50 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http", // Zmieniono na http, ponieważ używasz HTTP dla panel.stalumo.pl
+        hostname: "panel.stalumo.pl",
+        port: "",
+        pathname: "/image-public-uploads/**",
+        search: "",
+      },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
