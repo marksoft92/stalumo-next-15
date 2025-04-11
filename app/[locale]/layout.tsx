f0eed5fd7114eadb3a1a3831995d6abc607873a1
Dawid Bieńkowski <marksoft1992@gmail.com>
2025-04-11 14:23:11 +0200

    fix Promise<Metadata>

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata({
   params,
 }: {
   params: { locale: Locale };
-}): Metadata {
+}): Promise<Metadata> {
   const { locale } = await params;
 
   const meta = {
