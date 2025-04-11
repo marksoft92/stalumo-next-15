a3972cb3a79e558ee6f2165ea0db597669dd15a1
Dawid Bieńkowski <marksoft1992@gmail.com>
2025-04-11 13:58:08 +0200

    added animation component

--- components/ScrollAnimation.tsx
+++ components/ScrollAnimation.tsx
@@ -1,4 +1,6 @@
 
+"use client"
+import React, { useEffect, useState } from 'react';
 
 interface ScrollAnimationProps {
     children: React.ReactNode;
@@ -24,46 +26,47 @@ const ScrollAnimation: React.FC<ScrollAnimationProps> = ({ children, direction }
     }, []);
 
     // Jeśli ekran jest mniejszy lub równy 1000px, nie uruchamiamy animacji
-    if (isMobile) {
-        return <div>{children}</div>;
-    }
+
 
     useEffect(() => {
-        const animateElements = document.querySelectorAll(`.anim-${direction}`);
+        if(!isMobile) {
+            const animateElements = document.querySelectorAll(`.anim-${direction}`);
 
-        const observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    // Usuń klasy transformacyjne w zależności od kierunku animacji
-                    if (direction === 'left') {
-                        entry.target.classList.remove('translate-x-full');
-                        entry.target.classList.add('translate-x-0');
-                    } else if (direction === 'right') {
-                        entry.target.classList.remove('translate-x-full');
-                        entry.target.classList.add('translate-x-0');
-                    } else if (direction === 'top') {
-                        entry.target.classList.remove('translate-y-full');
-                        entry.target.classList.add('translate-y-0');
-                    } else if (direction === 'bottom') {
-                        entry.target.classList.remove('translate-y-full');
-                        entry.target.classList.add('translate-y-0');
+            const observer = new IntersectionObserver((entries, observer) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        // Usuń klasy transformacyjne w zależności od kierunku animacji
+                        if (direction === 'left') {
+                            entry.target.classList.remove('translate-x-full');
+                            entry.target.classList.add('translate-x-0');
+                        } else if (direction === 'right') {
+                            entry.target.classList.remove('translate-x-full');
+                            entry.target.classList.add('translate-x-0');
+                        } else if (direction === 'top') {
+                            entry.target.classList.remove('translate-y-full');
+                            entry.target.classList.add('translate-y-0');
+                        } else if (direction === 'bottom') {
+                            entry.target.classList.remove('translate-y-full');
+                            entry.target.classList.add('translate-y-0');
+                        }
+
+                        // Włącz animację zmieniając opacity
+                        entry.target.classList.remove('opacity-0');
+                        entry.target.classList.add('opacity-100');
+
+                        // Przestań obserwować element
+                        observer.unobserve(entry.target);
                     }
-
-                    // Włącz animację zmieniając opacity
-                    entry.target.classList.remove('opacity-0');
-                    entry.target.classList.add('opacity-100');
-
-                    // Przestań obserwować element
-                    observer.unobserve(entry.target);
-                }
+                });
+            }, {
+                threshold: 0.3, // Animacja po 50% widoczności elementu
             });
-        }, {
-            threshold: 0.3, // Animacja po 50% widoczności elementu
-        });
 
-        animateElements.forEach((element) => {
-            observer.observe(element);
-        });
+            animateElements.forEach((element) => {
+                observer.observe(element);
+            });
+        }
+
     }, [direction]);
 
     return (
