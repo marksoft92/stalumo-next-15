891c5b5e5095d4519aa36372f2291469fce6e250
Dawid Bieńkowski <marksoft1992@gmail.com>
2025-04-15 13:01:58 +0200

    fix protocol for images

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http", // Zmieniono na http, ponieważ używasz HTTP dla panel.stalumo.pl
+        protocol: "https", // Zmieniono na http, ponieważ używasz HTTP dla panel.stalumo.pl
         hostname: "panel.stalumo.pl",
         port: "",
         pathname: "/image-public-uploads/**",
