ecbf210ca8a43f3feb320ae3a089d7a9eb552653
Dawid BIeńkowski <marksoft1992@gmail.com>
2025-06-11 23:26:57 +0200

    js to ts

---
+++ next-sitemap.config.ts
@@ -0,0 +1,26 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.APP_URL || "https://stalumo.com",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 0.7,
+  exclude: ['/secret', '/api/*'],
+  additionalSitemaps: [
+    `${process.env.APP_URL || 'https://stalumo.com'}/sitemap-blog.xml`,
+  ],
+  alternateRefs: [
+    {
+      href: `${process.env.APP_URL || 'https://stalumo.com'}/pl`,
+      hreflang: "pl",
+    },
+    {
+      href: `${process.env.APP_URL || 'https://stalumo.com'}/en`,
+      hreflang: "en",
+    },
+    {
+      href: `${process.env.APP_URL || 'https://stalumo.com'}/de`,
+      hreflang: "de",
+    },
+  ],
+};
