4b05bba08a48d7d622ca96f18b552784d23f7b49
Dawid <dawid.bienkowski@byss.pl>
2025-07-15 08:29:39 +0200

    set new form

--- app/[locale]/contact/ContactForm.tsx
+++ app/[locale]/contact/ContactForm.tsx
@@ -10,6 +10,12 @@ export default function ContactForm(locale: any) {
   const [email, setEmail] = useState("");
   const [topic, setTopic] = useState("");
   const [content, setContent] = useState("");
+  const [category, setCategory] = useState("");
+  const [project, setProject] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [assembly, setAssembly] = useState("");
+  const [phone, setPhone] = useState("");
+  const [startDate, setStartDate] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
@@ -34,13 +40,26 @@ export default function ContactForm(locale: any) {
 
     try {
       const recaptchaToken = await executeRecaptcha("contact_form");
+      const generatedString = `
+${t("form.summary.category")}: ${category}
+${t("form.summary.project")}: ${project}
+${t("form.summary.quantity")}: ${quantity}
+${t("form.summary.assembly")}: ${assembly}
+${t("form.summary.phone")}: ${phone}
+${t("form.summary.date")}: ${startDate}
+`.trim();
+
+// Finalny content
+const finalContent = content + "\n\n" + generatedString;
+
 
       const response = await fetch("/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, topic, content, recaptchaToken }),
+        body: JSON.stringify({ email, topic, content: finalContent, recaptchaToken }),
+
       });
 
       const result = await response.json();
@@ -62,7 +81,7 @@ export default function ContactForm(locale: any) {
     <>
 
     <Container>
-      <div className="flex max-lg:flex-col w-full gap-10 lg:h-[60vh]">
+      <div className="flex max-lg:flex-col w-full gap-10 ">
         <form
           onSubmit={handleSubmit}
           className="space-y-6 text-[#000] lg:w-[50%] "
@@ -86,7 +105,16 @@ export default function ContactForm(locale: any) {
               className="mt-1 block w-full px-4 py-3 border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
             />
           </div>
-
+          <div>
+  <label className="block text-sm font-medium text-[#fff]">{t("form.phone")}</label>
+  <input
+    type="tel"
+    onChange={(e) => setPhone(e.target.value)}
+    value={phone}
+    required
+    className="mt-1 block w-full px-4 py-3 border border-gray-300 shadow-sm sm:text-sm rounded-md"
+  />
+</div>
           <div>
             <label
               htmlFor="email"
@@ -122,6 +150,84 @@ export default function ContactForm(locale: any) {
               className="mt-1 block w-full px-4 py-3 border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
             ></textarea>
           </div>
+
+
+
+
+          <div>
+  <label className="block text-sm font-medium text-[#fff]">{t("form.category")}</label>
+  <select
+    onChange={(e) => setCategory(e.target.value)}
+    value={category}
+    required
+    className="mt-1 block w-full px-4 py-3 border border-gray-300 shadow-sm sm:text-sm rounded-md"
+  >
+    <option value="">{t("form.select")}</option>
+    <option value={t("form.balustrades")}>{t("form.balustrades")}</option>
+    <option value={t("form.gates")}>{t("form.gates")}</option>
+    <option value={t("form.fences")}>{t("form.fences")}</option>
+    <option value={t("form.wickets")}>{t("form.wickets")}</option>
+    <option value={t("form.structures")}>{t("form.structures")}</option>
+  </select>
+</div>
+
+<div>
+  <label className="block text-sm font-medium text-[#fff]">{t("form.project")}</label>
+  <select
+    onChange={(e) => setProject(e.target.value)}
+    value={project}
+    required
+    className="mt-1 block w-full px-4 py-3 border border-gray-300 shadow-sm sm:text-sm rounded-md"
+  >
+    <option value="">{t("form.select")}</option>
+    <option value={t("form.yes")}>{t("form.yes")}</option>
+    <option value={t("form.no")}>{t("form.no")}</option>
+  </select>
+</div>
+
+<div>
+  <label className="block text-sm font-medium text-[#fff]">{t("form.quantity")}</label>
+  <input
+    type="text"
+    onChange={(e) => setQuantity(e.target.value)}
+    value={quantity}
+    required
+    className="mt-1 block w-full px-4 py-3 border border-gray-300 shadow-sm sm:text-sm rounded-md"
+  />
+</div>
+
+<div>
+  <label className="block text-sm font-medium text-[#fff]">{t("form.assembly")}</label>
+  <select
+    onChange={(e) => setAssembly(e.target.value)}
+    value={assembly}
+    required
+    className="mt-1 block w-full px-4 py-3 border border-gray-300 shadow-sm sm:text-sm rounded-md"
+  >
+    <option value="">{t("form.select")}</option>
+    <option value={t("form.withInstallation")}>{t("form.withInstallation")}</option>
+    <option value={t("form.noInstallation")}>{t("form.noInstallation")}</option>
+  </select>
+</div>
+
+
+
+<div>
+  <label className="block text-sm font-medium text-[#fff]">{t("form.date")}</label>
+  <input
+  type="date"
+  onChange={(e) => setStartDate(e.target.value)}
+  onClick={(e) => e.currentTarget.showPicker?.()} // pokazuje picker jeśli dostępny
+  value={startDate}
+  required
+  className="mt-1 block w-full px-4 py-3 border border-gray-300 shadow-sm sm:text-sm rounded-md cursor-pointer"
+/>
+
+</div>
+
+
+
+
 
           <div>
             <button
