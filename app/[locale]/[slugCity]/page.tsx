a848373a158c368ae669183822c3d5eb8aa92afd
Dawid <dawid.bienkowski@byss.pl>
2025-11-24 13:19:04 +0100

    fix 404

--- app/[locale]/[slugCity]/page.tsx
+++ app/[locale]/[slugCity]/page.tsx
@@ -22,35 +22,35 @@ export default async function Page({ params }: any) {
   const serviceEntry = services.find(s => s.service_slug === serviceSlug);
   const cityEntry = cities.find((c: any) => c.slugCity === citySlug);
 
-  if (!serviceEntry || !cityEntry) notFound();
+  
 
   // Podmiana placeholderów {city} i {defCity}
   const entry = {
     ...serviceEntry,
-    description: serviceEntry.description.replace("{defCity}", cityEntry.defCity).replace("{city}", cityEntry.city),
+    description: serviceEntry?.description.replace("{defCity}", cityEntry?.defCity ?? "").replace("{city}", cityEntry?.city ?? ""),
     seo: {
-      title: serviceEntry.seo.title.replace("{city}", cityEntry.city),
-      description: serviceEntry.seo.description.replace("{city}", cityEntry.city),
-      keywords: serviceEntry.seo.keywords.replace("{city}", cityEntry.city)
+      title: serviceEntry?.seo.title.replace("{city}", cityEntry?.city ?? ""),
+      description: serviceEntry?.seo.description.replace("{city}", cityEntry?.city ?? ""),
+      keywords: serviceEntry?.seo.keywords.replace("{city}", cityEntry?.city ?? "")
     },
-    faq: serviceEntry.faq.map(faq => ({
-      question: faq.question.replace("{defCity}", cityEntry.defCity).replace("{city}", cityEntry.city),
-      answer: faq.answer.replace("{defCity}", cityEntry.defCity).replace("{city}", cityEntry.city)
+    faq: serviceEntry?.faq.map(faq => ({
+      question: faq.question.replace("{defCity}", cityEntry?.defCity ?? "").replace("{city}", cityEntry?.city ?? ""),
+      answer: faq.answer.replace("{defCity}", cityEntry?.defCity ?? "").replace("{city}", cityEntry?.city ?? "")
     })),
 
-    review: serviceEntry.review.map(rev => ({
-      comment: rev.comment.replace("{defCity}", cityEntry.city).replace("{city}", cityEntry.city),
-      address: rev.address.replace("{defCity}", cityEntry.city).replace("{city}", cityEntry.city),
-      data: rev.data.replace("{defCity}", cityEntry.city).replace("{city}", cityEntry.city),
-      author: rev.author.replace("{defCity}", cityEntry.city).replace("{city}", cityEntry.city),
+    review: serviceEntry?.review.map(rev => ({
+      comment: rev.comment.replace("{defCity}", cityEntry?.city ?? "").replace("{city}", cityEntry?.city ?? ""),
+      address: rev.address.replace("{defCity}", cityEntry?.city ?? "").replace("{city}", cityEntry?.city ?? ""),
+      data: rev.data.replace("{defCity}", cityEntry?.city ?? "").replace("{city}", cityEntry?.city ?? ""),
+      author: rev.author.replace("{defCity}", cityEntry?.city ?? "").replace("{city}", cityEntry?.city ?? ""  ),
     })),
-    defCity: cityEntry.defCity,
-    slugCity: cityEntry.slugCity,
-    city: cityEntry.city,
-    regio: cityEntry.regio,
-    regioDef: cityEntry.regioDef,
-    slugRegio: cityEntry.slugRegio,
-    fromCity: cityEntry.fromCity
+    defCity: cityEntry?.defCity ?? "",
+    slugCity: cityEntry?.slugCity ?? "",
+    city: cityEntry?.city ?? "",
+    regio: cityEntry?.regio ?? "" ,
+    regioDef: cityEntry?.regioDef ?? "",
+    slugRegio: cityEntry?.slugRegio ?? "",
+    fromCity: cityEntry?.fromCity ?? ""
   };
 
 
